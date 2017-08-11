3fb8caaef52843d8630108b103bf1c00a67bb494
Denbora <denborag@gmail.com>
2017-08-11 18:26:16 +0300

    [Client - wrapper for SoapClient] #RTGS-1

---
+++ src/Client.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: denbora
+ * Date: 11.08.17
+ * Time: 18:21
+ */
+
+namespace denbora\R_T_G_Services;
+
+use SoapClient;
+
+class Client
+{
+    /**
+     * @var SoapClient
+     */
+    protected $soapClient;
+
+    public function __construct(SoapClient $soapClient)
+    {
+        $this->soapClient = $soapClient;
+    }
+
+    /**
+     * @return SoapClient
+     */
+    public function getSoapClient(): SoapClient
+    {
+        return $this->soapClient;
+    }
+
+}
