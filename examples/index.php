7ac9d162852f9fc4c32ad9bd22d4b70a6399ee29
Denbora <denborag@gmail.com>
2017-08-21 19:18:38 +0300

    validate credentials example - response handler needed

--- examples/index.php
+++ examples/index.php
@@ -9,8 +9,8 @@ require_once __DIR__ . '/../experiments/config.php';
 
 $casino = new Casino($base_url, $certificate, $rtgPassword);
 
-$method = 'createTokenByApp';
-$service = 'Security';
+$method = 'validateCredentials';
+$service = 'Player';
 
 try {
     Loader::call($service, $method, $casino);
