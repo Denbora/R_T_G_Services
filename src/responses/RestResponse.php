3f1885d5ddc01533489de5eb76e66d521e8af91e
Saur4ig <saurons909@gmail.com>
2017-09-01 16:20:48 +0300

    RTGS-20 Error Codes for Exception added new response logic using codelist added codes.json with all RTGs codes and messages

--- src/responses/RestResponse.php
+++ src/responses/RestResponse.php
@@ -7,6 +7,52 @@ use denbora\R_T_G_Services\R_T_G_ServiceException;
 class RestResponse implements ResponseInterface
 {
     /**
+     * array with RTG error codes and messages
+     *
+     * @var array
+     */
+    private $codeList;
+
+    /**
+     * Requested method
+     *
+     * @var string
+     */
+    private $method;
+
+    public function __construct()
+    {
+        $this->codeList = json_decode(file_get_contents(__DIR__ . '/../config/codes.json', true), true);
+    }
+
+    /**
+     * get error from codeList using code
+     *
+     * @param string $code
+     * @param string $uri
+     * @return string
+     */
+    private function getError($code, $uri)
+    {
+        $pattern = "/api/";
+        $data = explode($pattern, $uri);
+        $category = explode('/', $data[1]);
+        $categoryName = $category[0];
+
+        $action = explode('?', end($category))[0];
+        if ($action == '') {
+            $action = '/';
+        }
+
+        $message = $this->codeList[$categoryName][$action][$code];
+
+        if (is_null($message) && $action == '/') {
+            $message = $this->codeList[$categoryName][$action][$this->method][$code];
+        }
+        return $message;
+    }
+
+    /**
      * @param $response
      * @return mixed
      * @throws R_T_G_ServiceException
@@ -14,7 +60,13 @@ class RestResponse implements ResponseInterface
     public function getContent($response)
     {
         if (is_int($response->code) && $response->code != 200) {
-            throw new R_T_G_ServiceException('RTG Code - ' . $response->code);
+            if ($response->code != 500) {
+                $this->method = $response->request->method;
+                $message = $this->getError($response->code, $response->request->uri);
+            } else {
+                $message = 'Error in request execution. Please contact Support.';
+            }
+            throw new R_T_G_ServiceException('RTG Messaage - ' . $message);
         } else {
             return $response->body;
         }
