8b2b0ead10c8850a0941982d4c643cafdb6edd19
Saur4ig <saurons909@gmail.com>
2017-09-21 17:09:14 +0300

    RTGS-affiliate fixed und index

--- examples/index.php
+++ examples/index.php
@@ -8,12 +8,12 @@ use denbora\R_T_G_Services\examples\Loader;
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../experiments/config.php';
 
-//$url = $clear_url;
-$url = $base_url;
+$url = $clear_url;
+//$url = $base_url;
 $casino = new Casino($url, $certificate, $rtgPassword);
 //$casinoRest = new CasinoRest($restBaseUrl, $crt, $key, $rtgPassword);
-$method = 'getDownloadInformationByDates';
-$service = 'Download';
+$method = 'listGlobalLinked';
+$service = 'Affiliate';
 /*$method = 'putEmailVerificationStatus';
 $service = 'Player';*/
 
