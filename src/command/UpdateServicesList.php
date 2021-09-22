91988970e236b73fb0ef85292f8e969484507520
emmett <emmett@solidpower.email>
2021-09-22 16:22:01 +0300

    added handling for replace method names by API path. generated Rest V3 API services list.

--- src/command/UpdateServicesList.php
+++ src/command/UpdateServicesList.php
@@ -11,8 +11,16 @@ use Exception;
 
 class UpdateServicesList extends Command
 {
-    const SERVICES_LIST = __DIR__ . '/../config/restMethodsV2.json';
-    const RESPONSE_CODES = __DIR__ . '/../config/codes_v2.json';
+    const SERVICES_LIST = __DIR__ . '/../config/restMethodsV3.json';
+    const RESPONSE_CODES = __DIR__ . '/../config/codes_v3.json';
+    const FORCE_METHOD_NAMES = [
+        '/api/games/pending-games' => 'ClosePendingGamesByList',
+        '/api/reports/transactions-per-skin' => 'GetPlayerTransactionsPerSkin',
+        '/api/v2/accounts/validate-credentials' => 'ValidateCredentialsV2',
+        '/api/v2/jackpots/lobby-progressive-jackpot' => 'GetLobbyGetProgressiveJackpotV2',
+        '/api/v2/players/{playerId}/coupons/active' => 'GetActiveCouponDetailsByPlayerV2',
+    ];
+
 
     protected static $defaultName = 'app:update-services-list';
 
@@ -42,57 +50,27 @@ class UpdateServicesList extends Command
                     foreach ($methods as $methodType => $method) {
                         list($categoryName, $methodName) = explode('_', $method['operationId']);
 
-                        if (in_array($method['operationId'], $listOfOperationIds)) {
-                            throw new ParseAPIException(
-                                sprintf("Duplicate method  name \"%s\" in \"%s\" category", $methodName, $categoryName)
-                            );
-                        }
+                        $methodName = $this->replaceMethodName($pathTemplate, $methodName);
 
-                        $listOfOperationIds[] = $method['operationId'];
-
-                        $parameters = [];
-                        if (isset($method['parameters'])) {
-                            foreach ($method['parameters'] as $parameter) {
-                                if (!isset($parameter['schema'])) {
-                                    $parameters[$parameter['name']] = [
-                                        'dataType' => $parameter['type'],
-                                        'parameterType' => $parameter['in'],
-                                        'required' => $parameter['required'],
-                                        'description' => $parameter['description'] ?? ''
-                                    ];
-                                } else {
-                                    $parameters[$parameter['name']] = 'JSON';
-                                }
-                            }
-                        }
-
-                        $responses = [];
-                        foreach ($method['responses'] as $code => $response) {
-                            $responses[$code] = $response['description'];
-                        }
-
-                        $deprecated = "";
-                        if (isset($method['deprecated']) && true === $method['deprecated']) {
-                            $deprecated = $this->getDeprecationInfo($method['description'], $methodType);
-                        }
+                        $this->checkDuplicateMethodName($listOfOperationIds, $categoryName, $methodName);
 
                         $services[$categoryName][$methodName] = [
                             'method' => strtoupper($methodType),
                             'path' => $pathTemplate,
-                            'parameters' => $parameters ?? [],
-                            'responses' => $responses ?? [],
-                            'deprecated' => $deprecated,
+                            'parameters' => $this->getMethodParameters($method),
+                            'responses' => $this->getMethodResponses($method),
+                            'deprecated' => $this->getDeprecationInfo($method, $methodType),
                         ];
 
                         $queryPathTemplate = strtoupper($methodType) . ' ' . $pathTemplate;
-                        $responseCodes[$queryPathTemplate] = $responses ?? [];
+                        $responseCodes[$queryPathTemplate] = $services[$categoryName][$methodName]['responses'];
                     }
                 }
             }
         } catch (ParseAPIException $exception) {
             dd($exception->getMessage());
-        }catch (Exception $exception) {
-            dd($exception, $response ?? null, $method);
+        } catch (Exception $exception) {
+            dd($exception, $method);
         }
 
         return [
@@ -102,17 +80,98 @@ class UpdateServicesList extends Command
     }
 
     /**
-     * @param string $description
+     * Force API method name to be replaced if needed
+     * @param string $pathTemplate
+     * @param string $methodName
+     * @return string
+     */
+    private function replaceMethodName(string $pathTemplate, string $methodName): string
+    {
+        $result = $methodName;
+        if (array_key_exists($pathTemplate, self::FORCE_METHOD_NAMES)) {
+            $result = self::FORCE_METHOD_NAMES[$pathTemplate];
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array $method
+     * @return array
+     */
+    private function getMethodResponses(array $method): array
+    {
+        $responses = [];
+        foreach ($method['responses'] as $code => $response) {
+            $responses[$code] = $response['description'];
+        }
+
+        return $responses;
+    }
+
+    /**
+     * @param array $method
+     * @return array
+     */
+    private function getMethodParameters(array $method): array
+    {
+        $result = [];
+        if (isset($method['parameters'])) {
+            foreach ($method['parameters'] as $parameter) {
+                if (!isset($parameter['schema'])) {
+                    $result[$parameter['name']] = [
+                        'dataType' => $parameter['type'],
+                        'parameterType' => $parameter['in'],
+                        'required' => $parameter['required'],
+                        'description' => $parameter['description'] ?? ''
+                    ];
+                } else {
+                    $result[$parameter['name']] = 'JSON';
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array $listOfOperationIds
+     * @param string $categoryName
+     * @param string $methodName
+     * @throws ParseAPIException
+     */
+    private function checkDuplicateMethodName(
+        array &$listOfOperationIds,
+        string $categoryName,
+        string $methodName
+    ) {
+        $operationId = $categoryName . "_" . $methodName;
+        if (in_array($operationId, $listOfOperationIds)) {
+            throw new ParseAPIException(
+                sprintf("Duplicate method  name \"%s\" in \"%s\" category", $methodName, $categoryName)
+            );
+        }
+        $listOfOperationIds[] = $operationId;
+    }
+
+    /**
+     * @param array $method
      * @param string $methodType
      * @return string
      */
-    private function getDeprecationInfo(string $description, string $methodType): string
+    private function getDeprecationInfo(array $method, string $methodType): string
     {
-        $deprecated = "";
+        $result = "";
+        if (!isset($method['deprecated']) || true !== $method['deprecated']) {
+            return $result;
+        }
 
-        preg_match('/(?<=<a href="#!\/)(.*)(?=">~)/', $description, $matches);
+        preg_match('/(?<=<a href="#!\/)(.*)(?=">)/', $method['description'], $matches);
         if (count($matches)) {
             $deprecationUseClass = current($matches);
+
+            preg_match('/(?=\/api\/)(.*)(?=<\/a>)/', $method['description'], $matchesUrl);
+            $deprecationUrl = count($matchesUrl) ? current($matchesUrl) : "";
 
             $isRestV3 = strpos(
                 $deprecationUseClass,
@@ -124,11 +183,12 @@ class UpdateServicesList extends Command
             list($serviceName, $methodName) = explode('/', str_replace('/v3/', '/', $deprecationUseClass));
 
             $methodName = str_replace($serviceName . "_", "", $methodName);
+            $methodName = $this->replaceMethodName($deprecationUrl, $methodName);
             $methodName = lcfirst($methodName) . strtoupper($methodType);
 
-            $deprecated = "Use {@see " . $serviceNamespace . $serviceName . 'Service' . "::" . $methodName . "()}";
+            $result = "Use {@see " . $serviceNamespace . $serviceName . 'Service' . "::" . $methodName . "()}";
         }
 
-        return $deprecated;
+        return $result;
     }
 }
